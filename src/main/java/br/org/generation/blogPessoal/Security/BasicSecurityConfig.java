cce308af9fba73a2ac9070b0cbc953ba604ef004
kaique-mota <kaiquecarvalho42724@gmail.com>
2021-08-10 14:49:18 -0300

    corrigindo basic security

--- src/main/java/br/org/generation/blogPessoal/Security/BasicSecurityConfig.java
+++ src/main/java/br/org/generation/blogPessoal/Security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package br.org.generation.blogPessoal.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,8 +36,17 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
+        .antMatchers(HttpMethod.GET, "/postagem").permitAll()
+        .antMatchers(HttpMethod.GET, "/tema").permitAll()
+        .antMatchers(HttpMethod.POST, "/postagem").permitAll()
+        .antMatchers(HttpMethod.POST, "/tema").permitAll()
+        .antMatchers(HttpMethod.PUT, "/postagem").permitAll()
+        .antMatchers(HttpMethod.PUT, "/tema").permitAll()
+        .antMatchers(HttpMethod.DELETE, "/postagem").permitAll()
+        .antMatchers(HttpMethod.DELETE, "/tema").permitAll()
  		.anyRequest().authenticated()
  		.and().httpBasic()
  		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
